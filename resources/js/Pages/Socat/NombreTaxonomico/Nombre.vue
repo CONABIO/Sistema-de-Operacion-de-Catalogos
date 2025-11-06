8547497e6ed801bd47932c7d20dde27f352cf820
Raciel tre <anael-tarma@hotmail.com>
2025-11-06 12:42:00 -0600

    Se agregan las animaciones de loading en el cerrado de las pantallas

--- resources/js/Pages/Socat/NombreTaxonomico/Nombre.vue
+++ resources/js/Pages/Socat/NombreTaxonomico/Nombre.vue
@@ -191,7 +191,28 @@ const resetFormNombre = () => {
 
 const closeDialog = () => {
   dialogFormVisibleAlta.value = false;
+};
+
+const closeDialogRel = async() => {
+  const loading = ElLoading.service({
+        lock: true,
+        text: "Loading",
+        spinner: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><path fill="none" d="M0 0h200v200H0z"></path><path fill="none" stroke-linecap="round" stroke="#53B0FF" stroke-width="15" transform-origin="center" d="M70 95.5V112m0-84v16.5m0 0a25.5 25.5 0 1 0 0 51 25.5 25.5 0 0 0 0-51Zm36.4 4.5L92 57.3M33.6 91 48 82.7m0-25.5L33.6 49m58.5 33.8 14.3 8.2"><animateTransform type="rotate" attributeName="transform" calcMode="spline" dur="1.1" values="0;-120" keyTimes="0;1" keySplines="0 0 1 1" repeatCount="indefinite"></animateTransform></path><path fill="none" stroke-linecap="round" stroke="#53B0FF" stroke-width="15" transform-origin="center" d="M130 155.5V172m0-84v16.5m0 0a25.5 25.5 0 1 0 0 51 25.5 25.5 0 0 0 0-51Zm36.4 4.5-14.3 8.3M93.6 151l14.3-8.3m0-25.4L93.6 109m58.5 33.8 14.3 8.2"><animateTransform type="rotate" attributeName="transform" calcMode="spline" dur="1.1" values="0;120" keyTimes="0;1" keySplines="0 0 1 1" repeatCount="indefinite"></animateTransform></path></svg>`,
+        backgroud: 'rgba(255,255,255,0.85)',
+      });
+
+  const params= {
+                  taxAct: taxonAct.value.id
+                };
+
+  const response = await axios.get('/carga-RelacionesTax', { params });
+
+  tablaNomenclatura.value = response.data;
+
   dialogFormVisibleRel.value = false;
+
+  loading.close();
+
 };
 
 const cerrarDialog = (valor) => {
@@ -1076,7 +1097,8 @@ const abre_Relaciones = () => {
     <DialogForm v-model="dialogFormVisibleRel" :botCerrar="true" :pressEsc="true" :width="'83%'"
       custom-class="responsive-dialog relations-dialog">
       <DialogRelaciones :taxonAct="taxonAct" :gruposTax="gruposTax" :categoriasTax="categoriasTax"
-        :catalogPadre="catalogos" :gruposPadre="grupos" :idsGruposPadre="idsGrupos" @cerrar="closeDialog">
+        :catalogPadre="catalogos" :gruposPadre="grupos" :idsGruposPadre="idsGrupos" 
+        @cerrar="closeDialogRel">
       </DialogRelaciones>
     </DialogForm>
 
