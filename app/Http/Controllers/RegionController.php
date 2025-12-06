f7afd7a047aa3c1c701629c245e8eeff65e66828
Leo020924 <leonardovazquezoviedo@gmail.com>
2025-12-05 18:11:56 -0600

    Subiendo cambios de catalogo de Regiones y Tipos de Regiones

--- app/Http/Controllers/RegionController.php
+++ app/Http/Controllers/RegionController.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\DB;
 
 class RegionController extends Controller
 {
@@ -19,8 +20,8 @@ class RegionController extends Controller
         $tiposDeRegionTree = $this->buildTipoRegionTree($todosLosTiposDeRegion);
         return Inertia::render('Socat/Regiones/indexRegion', [
             'treeDataProp' => $treeData,
-            'tiposDeRegionProp' => $todosLosTiposDeRegion, 
-            'tiposDeRegionTreeProp' => $tiposDeRegionTree, 
+            'tiposDeRegionProp' => $todosLosTiposDeRegion,
+            'tiposDeRegionTreeProp' => $tiposDeRegionTree,
         ]);
     }
 
@@ -64,54 +65,12 @@ class RegionController extends Controller
             if ($parentKey && isset($nodeMap[$parentKey])) {
                 $nodeMap[$parentKey]['children'][] = &$node;
             } else {
-                $tree[] = &$node; // Si no tiene padre, es raíz
+                $tree[] = &$node;
             }
         }
         return $tree;
     }
 
-    /* // Helper para la construcción del árbol de Tipos de Región
-    private function isDirectChild($item, $allItems, $currentLevel) {
-        $parentLevel = $currentLevel - 1;
-        $parentLevelKey = 'Nivel' . $parentLevel;
-        $currentLevelKey = 'Nivel' . $currentLevel;
-
-        // Raíz
-        if ($currentLevel === 1) {
-            return $item->Nivel2 == 0;
-        }
-
-        // Buscar el padre potencial
-        foreach($allItems as $potentialParent) {
-            $isParent = true;
-            for ($i = 1; $i <= $parentLevel; $i++) {
-                if ($potentialParent->{'Nivel'.$i} != $item->{'Nivel'.$i}) {
-                    $isParent = false;
-                    break;
-                }
-            }
-             // El padre no debe tener niveles más allá de su propio nivel
-            if ($isParent && $potentialParent->{'Nivel'.($parentLevel + 1)} == 0) {
-                 // El item actual debe ser un hijo directo (no tener nietos en su definición)
-                if($item->{'Nivel'.($currentLevel + 1)} == 0) {
-                    return true;
-                }
-            }
-        }
-        
-        // Corrección para encontrar nodos hijos que a su vez son padres
-        $isParentCandidate = true;
-        for ($i = $currentLevel + 1; $i <= 5; $i++) {
-            if ($item->{'Nivel'.$i} != 0) {
-                $isParentCandidate = false;
-                break;
-            }
-        }
-        if($isParentCandidate) return true;
-
-        return false;
-    }
- */
 
     private function getParentKeyFromNodeKey($key)
     {
@@ -131,7 +90,7 @@ class RegionController extends Controller
             if ($node->{"Nivel{$i}"} > 0) {
                 $levels[] = $node->{"Nivel{$i}"};
             } else {
-                break; 
+                break;
             }
         }
         return 'L' . implode('_', $levels);
@@ -149,22 +108,40 @@ class RegionController extends Controller
             'idNodoReferencia' => 'nullable|integer',
         ]);
 
-        $idPadre = 0;
+        DB::transaction(function () use ($validatedData) {
 
-        if ($validatedData['opcionNivel'] === 'inferior') {
-            $idPadre = $validatedData['idNodoReferencia'];
-        } elseif ($validatedData['opcionNivel'] === 'mismo') {
-            $nodoHermano = Region::find($validatedData['idNodoReferencia']);
-            $idPadre = $nodoHermano ? $nodoHermano->IdRegionAsc : 0;
-        }
+            $idPadreInicial = null;
+            $esRaizAutoReferenciada = false;
 
-        Region::create([
-            'NombreRegion' => $validatedData['NombreRegion'],
-            'IdTipoRegion' => $validatedData['IdTipoRegion'],
-            'ClaveRegion' => $validatedData['ClaveRegion'],
-            'Abreviado' => $validatedData['Abreviado'],
-            'IdRegionAsc' => $idPadre,
-        ]);
+            if ($validatedData['opcionNivel'] === 'inferior') {
+                $idPadreInicial = $validatedData['idNodoReferencia'];
+            } elseif ($validatedData['opcionNivel'] === 'mismo') {
+                $nodoHermano = Region::find($validatedData['idNodoReferencia']);
+
+                if ($nodoHermano) {
+                    if ($nodoHermano->IdRegion == $nodoHermano->IdRegionAsc) {
+                        $esRaizAutoReferenciada = true;
+                        $idPadreInicial = $nodoHermano->IdRegion;
+                    } else {
+                        $idPadreInicial = $nodoHermano->IdRegionAsc;
+                    }
+                } else {
+                    $idPadreInicial = 1; 
+                }
+            }
+            $nuevaRegion = Region::create([
+                'NombreRegion' => $validatedData['NombreRegion'],
+                'IdTipoRegion' => $validatedData['IdTipoRegion'],
+                'ClaveRegion' => $validatedData['ClaveRegion'],
+                'Abreviado' => $validatedData['Abreviado'],
+                'IdRegionAsc' => $idPadreInicial,
+            ]);
+
+            if ($esRaizAutoReferenciada) {
+                $nuevaRegion->IdRegionAsc = $nuevaRegion->IdRegion;
+                $nuevaRegion->save();
+            }
+        });
 
         return redirect()->route('regiones.index')->with('success', 'Región creada.');
     }
