062e0be310c17675df5ea5198b77010ff959c7df
Leo020924 <leonardovazquezoviedo@gmail.com>
2026-07-24 11:37:58 -0600

    ADD: Subiendo cambios de filtros y estilos de TablaFiltrable

--- resources/js/Components/Biotica/LayoutCuerpo.vue
+++ resources/js/Components/Biotica/LayoutCuerpo.vue
@@ -20,6 +20,27 @@ defineProps({
 });
 </script>
 
+<style>
+:deep(.p-4) {
+    padding: 8px !important;
+}
+
+:deep(.min-h-\[90vh\]) {
+    min-height: auto !important;
+}
+
+:deep(.el-card__body) {
+    padding: 10px !important;
+}
+
+.main-section {
+    margin-top: 0 !important;
+}
+
+:deep(.el-collapse-item__content) {
+    padding-bottom: 5px !important;
+}</style>
+
 <template>
     <AppLayout v-if="usarAppLayout" :title="tituloPag">
         <div class="flex justify-start min-h-[90vh] p-4">
@@ -42,12 +63,12 @@ defineProps({
     <template v-else>
 
         <Head :title="tituloPag" />
-        <div class="flex justify-start min-h-[90vh] p-4">
-            <div class="w-full max-w-[1600px] mx-auto h-full">
+        <div class="flex justify-start min-h-[50vh] p-4">
+            <div class="w-full max-w-[1600px] mx-auto h-full ">
                 <Titulo :titulo="tituloArea" />
                 <br>
                 <div class="bg-white shadow-md sm:rounded-lg w-full flex flex-col">
-                    <div class="p-4 w-full h-full overflow=auto">
+                    <div class="p-1 w-full h-full overflow=auto">
                         <el-card class="box-card w-full h-full flex flex-col">
                             <div class="w-full h-full flex flex-col flex-grow">
                                 <slot></slot>
