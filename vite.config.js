042d087fa2de6c1a9146832a5686f66b7ea1b522
Leo020924 <leonardovazquezoviedo@gmail.com>
2026-09-07 18:29:59 -0600

    ADD: Modificaciones del JIRA SOCAT-31

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     server: {
-        host: '172.16.15.129',
+        host: '172.16.3.63',
         port: 5173,
         cors: {
-            origin: 'http://172.16.15.129:8001',
+            origin: 'http://172.16.3.63:8000',
         },
 
     },
